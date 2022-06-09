7f80816db59bb2bca08f93ae7002dcdcb004d5e0
romani4 <romani4armi@gmail.com>
2022-06-09 16:44:45 +0300

    adfad

--- project_final/js/icon.js
+++ project_final/js/icon.js
@@ -1,23 +1,34 @@
 
-var sneakersArr=[
-    {
-        imageTitle:'image/f1.png',
-        title:'Nike / Кроссовки WMNS NIKE SUPERREP GROOVE',
-        rating:'4.8',
-        review:'35',
-        price_1:'5650,00 p.',
-        price_2:'5820,00 p.',
-    },
-    ]
 
-//var ul=document.querySelector(".card_cneakers");
+var ul=document.querySelector(".card_cneakers");
 var html = '';
-
+var newSnekerV;
+var hitTovar;
+var discountSneaker;
 for(var i=0; i<sneakersArr.length; i++){
+   sneakersArr[i].newSneker==true?newSnekerV='style="display:block"':newSnekerV='';
+   sneakersArr[i].hit==true?hitTovar='style="display:block"':hitTovar='';
+   sneakersArr[i].discount>0?discountSneaker='style="display:block"':discountSneaker='';
     html+=`
-    
+    <li> 
+    <div  onclick="clickFunc" class="card_sneakers_background" style="${sneakersArr[i].background}">
+       
+        <div ${newSnekerV}>новинка</div>
+        <div  ${hitTovar} >хит</div>
+        <div  ${discountSneaker}> ${sneakersArr[i].discount}%</div>  
+        <div></div>  
+        <div  onclick="clickFunc"></div>
+    </div>  
+    <p>
+       <span>${sneakersArr[i].brend} </span> /${sneakersArr[i].description}
+    </p> 
+    <div> ${sneakersArr[i].price} р. </div>                       
+</li>
     `
     
 }
-console.log(html)
+
+function  clickFunc(){
+    alert('123')
+}
 ul.innerHTML = html;
